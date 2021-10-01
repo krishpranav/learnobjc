654033b7d824e3b1cbcbf7355bf460953e2c44a4
krishpranav <krisna.pranav@gmail.com>
2021-10-01 18:55:23 +0530

    learn to write if class + else class

--- ifelse/ifelse/ifelse.m
+++ ifelse/ifelse/ifelse.m
@@ -10,5 +10,18 @@
 
 int main()
 {
+    /* storing a int in value 'a'*/
+    int a = 100;
     
+    /* simple if class that will do
+     * if a is equal to 100 it will print 'you can see the text cause a is equal to 100'
+     * else you will see this text you see this text cause a is not 100
+     */
+    if (a == 100) {
+        NSLog(@"you can see the text cause a is equal to 100\n");
+    } else {
+        NSLog(@"you see this text cause a is not 100\n");
+    }
+    
+    return 0;
 }
