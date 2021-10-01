ba4f3e99e8cc003a9531adf265c5c1445c4e5dbf
krishpranav <krisna.pranav@gmail.com>
2021-10-01 18:35:18 +0530

    hello world in objective-c

--- helloworld/helloworld/main.m
+++ helloworld/helloworld/main.m
@@ -1,5 +1,18 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// imports
 #import <Foundation/Foundation.h>
 
+// main function
 int main() {
+    
+    // print hello world
+    NSLog(@"hello world\n");
+    
+    // returns 0
     return 0;
 }
