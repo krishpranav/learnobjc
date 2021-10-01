2fa9dbcec7e595509b86540af31c03cba59496c9
krishpranav <krisna.pranav@gmail.com>
2021-10-01 19:26:34 +0530

    forloop

--- forloop/forloop/main.m
+++ forloop/forloop/main.m
@@ -4,12 +4,26 @@
  * SPDX-License-Identifier: BSD-2-Clause
  */
 
+/* for loop execution */
+
 /* imports */
 #include <Foundation/Foundation.h>
 
-int main()
+int main ()
 {
-    NSLog("Hello world");
     
+    /* set an integer named 'a' */
+    int a;
+    
+    /* for a = 10; a less than 20 then increment a + 1
+     * and print the value
+    */
+    for( a = 10; a < 20; a = a + 1 ) {
+    
+        NSLog(@"value of a: %d\n", a);
+    }
+    
+    
+    /* returns 0 */
     return 0;
 }
