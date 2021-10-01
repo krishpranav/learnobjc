136f20e011a40a26c7e33e80deb965bb03525e1f
krishpranav <krisna.pranav@gmail.com>
2021-10-01 19:09:02 +0530

    bool

---
+++ bool/bool/bool.m
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#import <Foundation/Foundation.h>
+
+int main()
+{
+    BOOL boolValue = YES;
+    
+    NSLog(@"Bool value: %@", boolValue ? @"YES" : @"NO");
+    
+    return 0;
+}
