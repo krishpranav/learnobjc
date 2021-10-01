5a2741f09f99ae71dba6bbb78990a562a8bb712d
krishpranav <krisna.pranav@gmail.com>
2021-10-01 19:15:00 +0530

    strings

---
+++ strings/strings/main.m
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+/* import */
+#import <Foundation/Foundation.h>
+
+int main()
+{
+    /* storing world in  msg */
+    NSString *msg = @"World";
+    
+    /* and printing it out in NSLog */
+    NSLog(@"Hello: %@\n", msg);
+    
+    /* returns 0 */
+    return 0;
+}
