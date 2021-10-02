b73a85cbea368e8701271deb0f7ae19fc278e12c
krishpranav <krisna.pranav@gmail.com>
2021-10-02 16:20:24 +0530

    classes

---
+++ classes/classes/main.m
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Krisna Pranav
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+*/
+
+// imports
+#import <Foundation/Foundation.h>
+
+int main()
+{
+    NSLog(@"hello world");
+    
+    return 0;
+}
